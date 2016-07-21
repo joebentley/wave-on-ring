6485ed33d1f587c1c60bea1f30fd046ea2ae1e59
Joe Bentley <joebentley10@gmail.com>
2016-07-21 10:57:37 +0100

    Added non-integer slider

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+// Create the renderer
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff, 1);
 document.body.appendChild(renderer.domElement);
 
+// Generate and add the curve
 var geometry = newCurveGeometry(1);
 var material = new THREE.LineBasicMaterial({ color: 0xff0000, linewidth: 4 });
-
 var line = new THREE.Line(geometry, material);
 scene.add(line);
 
+// Generate axes and add them
 var axes = createCartesianAxes();
 for (var i = 0; i < axes.length; i++) {
-	line.add(axes[i]);
+	scene.add(axes[i]);
 }
 
+// Set camera and transform scene to a nice perspective
 camera.position.z = 50;
-line.rotation.x = 0.4;
-line.rotation.y = -0.8;
+scene.rotation.x = 0.4;
+scene.rotation.y = -0.8;
 
+// Main render function
 function render() {
     requestAnimationFrame(render);
     renderer.render(scene, camera);
 }
 render();
 
+// Create a new set of `n` points that the curve will pass through
 function getCurve(n, freq, phase) {
 	var res = [];
     var interval = 2 * Math.PI / n;
@@ -43,6 +48,7 @@ function getCurve(n, freq, phase) {
     return res;
 }
 
+// Generate a new Geometry from a CatmullRomCurve3 passing through points from getCurve
 function newCurveGeometry(freq, phase) {
     var curve = new THREE.CatmullRomCurve3(getCurve(200, freq, phase));
     curve.closed = true;
@@ -53,6 +59,7 @@ function newCurveGeometry(freq, phase) {
     return geometry;
 }
 
+// Generate Array of Object3Ds that represent the coordinate axes
 function createCartesianAxes() {
 	var l = 10;
 	var axes = [];
@@ -60,15 +67,15 @@ function createCartesianAxes() {
     	var geometry = new THREE.Geometry();
         var material;
         switch (i) {
-        case 0:
+        case 0: // x
         	geometry.vertices.push(new THREE.Vector3(0, 0, 0), new THREE.Vector3(l, 0, 0));
             material = new THREE.LineBasicMaterial({ color: 0xff0000 });
         	break;
-        case 1:
+        case 1: // y
         	geometry.vertices.push(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, l, 0));
             material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
         	break;
-        case 2:
+        case 2: // z
         	geometry.vertices.push(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, l));
             material = new THREE.LineBasicMaterial({ color: 0x0000ff });
         	break;
@@ -80,14 +87,27 @@ function createCartesianAxes() {
     return axes;
 }
 
+/* Page events */
+
 $('#frequency').on('input', function() {
 	var freq = this.value;
+	// Generate a new curve geometry and edit the current line Object3D with new frequency
 	scene.getObjectById(line.id).geometry = newCurveGeometry(freq, $('#phase').val() * 2 * Math.PI);
 	$('#frequency_value').text(freq);
 });
 
 $('#phase').on('input', function() {
 	var phase = this.value * 2 * Math.PI;
+	// Generate a new curve geometry and edit the current line Object3D with new phase
     scene.getObjectById(line.id).geometry = newCurveGeometry($('#frequency').val(), phase);
     $('#phase_value').text(this.value + ' * 2pi');
+});
+
+$('#allow_non_integer').change(function() {
+	// Change step size for the frequency slider to show non-integer discontinuity
+	if (this.checked) {
+		$('#frequency').prop('step', 0.1);
+	} else {
+		$('#frequency').prop('step', 1);
+	}
 });
